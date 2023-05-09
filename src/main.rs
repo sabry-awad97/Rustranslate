555dfd177cf4b5bb206bb9797523050cdcbdeb3c
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-09 07:51:35 +0300

    🚀 feat(main.rs): Add new `Translator` struct to handle translation requests The new `Translator` struct will hold information about the source and target languages for translations, as well as the `reqwest::Client` to send API requests. Changes made: - Added a new `Translator` struct with `source_lang`, `target_lang`, and `client` fields. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ use reqwest::Client;
 use std::io::{self, Write};
 use std::process;
 
+struct Translator {
+    source_lang: String,
+    target_lang: String,
+    client: Client,
+}
+
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
