d516eba0d0bcd2e18e1e4325f8a4e02e3907fb22
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-09 08:14:56 +0300

    🚀 chore: Remove unused dependency and add custom error enum This update removes the `anyhow` dependency, which was previously unused. Instead, a custom error enum called `TranslationError` was added to handle translation errors. The `fmt::Display` trait was implemented for `TranslationError` to allow for better error messaging. Changes made: - The `anyhow` dependency was removed from the `Cargo.toml` file. - A new `TranslationError` enum was created to handle translation errors. - The `fmt::Display` trait was implemented for `TranslationError` to allow for better error messaging. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
-use anyhow::{Context, Result};
 use rand::Rng;
 use reqwest::Client;
 use std::io::{self, Write};
-use std::process;
+use std::{fmt, process};
+
+enum TranslationError {
+    RequestFailed,
+    ResponseParsingFailed,
+    NoTranslationFound(String),
+    MaxRetriesExceeded(String),
+}
+
+impl fmt::Display for TranslationError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TranslationError::RequestFailed => write!(f, "Failed to send translation request"),
+            TranslationError::ResponseParsingFailed => {
+                write!(f, "Failed to parse response body as JSON")
+            }
+            TranslationError::NoTranslationFound(word) => {
+                write!(f, "No translation found for: {}", word)
+            }
+            TranslationError::MaxRetriesExceeded(message) => {
+                write!(f, "Max retries exceeded: {}", message)
+            }
+        }
+    }
+}
 
 struct Translator {
     source_lang: String,
