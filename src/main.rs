714407b66aaf93368ef02c1b58d5adaee0c7dab6
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-09 07:56:08 +0300

    🎉 feat(main.rs): Add Translator struct and new() method This commit adds a `new()` method to instantiate Translator struct. The `Translator` struct contains fields for `source_lang`, `target_lang`, and a `reqwest::Client` instance. The `new` function takes two parameters, `source_lang` and `target_lang`, which can be any type that can be converted into a `String` using the `Into` trait. This lays the foundation for implementing translation functionality in future commits.
    Changes made:
    - Added a new `Translator` struct that holds the `source_lang`, `target_lang` and a `client` field.
    - Added a `new` function to the `Translator` struct that takes two parameters, `source_lang` and `target_lang`.
    - `main()` function has been updated to create an instance of the `Translator` struct with source and target language codes.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,17 @@ struct Translator {
     client: Client,
 }
 
+impl Translator {
+    fn new(source_lang: impl Into<String>, target_lang: impl Into<String>) -> Self {
+        Self {
+            source_lang: source_lang.into(),
+            target_lang: target_lang.into(),
+            client: Client::new(),
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let translator = Translator::new("en", "es");
 }
